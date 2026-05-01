23f4414694748ee7ae265563f0b0ee64d948314c
Tatiana Sanchez <tatiana.sanchez.sanin@gmail.com>
2026-05-01 18:00:24 -0500

    Refactor application structure and integrate SonarQube scanner - Remove AppController and AppService - Introduce KafkaModule for Kafka integration - Add SonarQube scanner to package.json and package-lock.json - Create sonar-project.properties for SonarQube configuration - Add scanner lock and report task files

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { LogsController } from './logs/logs.controller';
+import { KafkaModule } from './kafka/kafka.module';
+import { LogsService } from './logs/logs.service';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [KafkaModule],
+  controllers: [LogsController],
+  providers: [LogsService],
 })
 export class AppModule {}
